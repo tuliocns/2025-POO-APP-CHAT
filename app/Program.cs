a989fa0dba5c66c8bec20106ef53f373e0cf2eae
Túlio <163613757+tuliocns@users.noreply.github.com>
2025-11-09 00:35:00 +0000

    2025-2-POO-APP-CHAT

--- app/Program.cs
+++ app/Program.cs
@@ -1,22 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Para qual canal você quer enviar mensagem?");
-Console.WriteLine("1. WhtasApp");
+Console.WriteLine("1. WhatsApp");
 Console.WriteLine("2. Telegram");
 Console.WriteLine("3. Instagram");
 Console.WriteLine("4. Facebook");
 
 int canal = 0;
-string resposta = Console.Read.Line();
+string resposta = Console.ReadLine();
 if (!int.TryParse(resposta, out canal))
 {
-    Console.WriteLine("Opção inválida!");
+    Console.WriteLine("Opção inválida");
     return;
 }
 
 TextMessage messageBase = new TextMessage("Olá");
+VideoMessage video = new VideoMessage("Olá");
 if(canal == 1)
 {
-    var whats =  new WhatsApp
-    whats.EnviarMensagem()
+    var whats = new WhatsApp();
+    whats.EnviarMensagem("12345678", messageBase);
+
+    whats.EnviarMensagem("87654321", video);
 }
 
